536ec771eb820383c281daa1c892a7f08608bf09
Ram Ratan Maurya <ratan.maurya@gmail.com>
2018-12-27 08:11:24 +0530

    Create analogwp-templates.php

---
+++ analogwp-templates.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: AnalogWP Templates
+ * Plugin URI:  https://analogwp.com/
+ * Description: Handfully crafted Elementor templates packs.
+ * Version:     0.0.1
+ * Author:      AnalogWP
+ * Author URI:  https://analogwp.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ang
+ */
+
