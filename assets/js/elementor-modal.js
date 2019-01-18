3d81c40125dcdc91b0a2654d70fb7ca5bc1abc6c
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-01-18 16:49:44 +0530

    Fix coding style for elementor-modal.js

--- assets/js/elementor-modal.js
+++ assets/js/elementor-modal.js
@@ -1,39 +1,40 @@
-'undefined' != typeof jQuery && ! (function ($) {
-	$(function () {
+/* global elementor, elementorCommon */
+'undefined' != typeof jQuery && ! ( function( $ ) {
+	$( function() {
 		function modal() {
-			elementorCommon && (window.analogModal || (window.analogModal = elementorCommon.dialogsManager.createWidget("lightbox", {
-				id: "analogwp-templates-modal",
+			elementorCommon && ( window.analogModal || ( window.analogModal = elementorCommon.dialogsManager.createWidget( 'lightbox', {
+				id: 'analogwp-templates-modal',
 				headerMessage: 'What is this???',
-				message: "",
+				message: '',
 				hide: {
-					auto: !1,
-					onClick: !1,
-					onOutsideClick: !1,
-					onOutsideContextMenu: !1,
-					onBackgroundClick: !0
+					auto: ! 1,
+					onClick: ! 1,
+					onOutsideClick: ! 1,
+					onOutsideContextMenu: ! 1,
+					onBackgroundClick: ! 0
 				},
 				position: {
-					my: "center",
-					at: "center"
+					my: 'center',
+					at: 'center'
 				},
-				onShow: function () {
-					const content = window.analogModal.getElements("content");
-					content.append('<div id="analogwp-templates"></div>');
+				onShow: function() {
+					const content = window.analogModal.getElements( 'content' );
+					content.append( '<div id="analogwp-templates"></div>' );
 				},
-				onHide: function () {}
-			}), window.analogModal.getElements("header").remove(), window.analogModal.getElements("message").append(window.analogModal.addElement("content"))), window.analogModal.show())
+				onHide: function() {}
+			}), window.analogModal.getElements( 'header' ).remove(), window.analogModal.getElements( 'message' ).append( window.analogModal.addElement( 'content' ) ) ), window.analogModal.show() );
 		}
 
 		window.analogModal = null;
 
-		const template = $("#tmpl-elementor-add-section");
+		const template = $( '#tmpl-elementor-add-section' );
 
-		if(template.length > 0 && typeof elementor !== undefined) {
+		if ( 0 < template.length && typeof elementor !== undefined ) {
 			let text = template.text();
 
-			text = text.replace('<div class="elementor-add-section-drag-title', '<div class="elementor-add-section-area-button elementor-add-analogwp-button" title="AnalogWP Templates"> <i class="fa fa-circle-o"></i></div> <div class="elementor-add-section-drag-title'), template.text(text), elementor.on("preview:loaded", function () {
-				$(elementor.$previewContents[0].body).on("click", ".elementor-add-analogwp-button", modal);
+			text = text.replace( '<div class="elementor-add-section-drag-title', '<div class="elementor-add-section-area-button elementor-add-analogwp-button" title="AnalogWP Templates"> <i class="fa fa-circle-o"></i></div> <div class="elementor-add-section-drag-title' ), template.text( text ), elementor.on( 'preview:loaded', function() {
+				$( elementor.$previewContents[0].body ).on( 'click', '.elementor-add-analogwp-button', modal );
 			});
 		}
 	});
-}(jQuery));
+}( jQuery ) );
