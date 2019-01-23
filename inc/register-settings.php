945496257c07147672f0032e2976b66fd16f3a3f
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-01-24 00:19:46 +0530

    Temporarily hide admin menu

--- inc/register-settings.php
+++ inc/register-settings.php
@@ -19,8 +19,7 @@ function register_menu() {
 	);
 }
 
-add_action( 'admin_menu', 'Analog\settings\register_menu' );
-
+// add_action( 'admin_menu', 'Analog\settings\register_menu' );
 function settings_page() {
 	do_action( 'ang_loaded' );
 	?>
