7a950fc9de3b349e99725aed921479f8da2ea289
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-01-29 01:20:13 +0530

    Added new notice icons

---
+++ src/js/app/icons/success.js
@@ -0,0 +1,7 @@
+const Success = () => (
+	<svg height="16" width="12" viewBox="0 0 12 16">
+		<path fillRule="evenodd" d="M12 5.5l-8 8-4-4L1.5 8 4 10.5 10.5 4 12 5.5z" />
+	</svg>
+);
+
+export default Success;
