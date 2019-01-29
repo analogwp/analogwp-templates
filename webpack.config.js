8a5ab76bec287053654fa76973dd1428c0634c15
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-01-29 14:35:16 +0530

    Fix syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
 /**
  * External Dependencies
  */
-const webpack = require("webpack");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const WebpackRTLPlugin = require("webpack-rtl-plugin");
+const webpack = require( 'webpack' );
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
 
 // Enviornment Flag
-const inProduction = "production" === process.env.NODE_ENV;
+const inProduction = 'production' === process.env.NODE_ENV;
 
 // Externals
 const externals = {
-	react: "React",
-	"react-dom": "ReactDOM"
+	react: 'React',
+	'react-dom': 'ReactDOM',
 };
 
 // Webpack config
 const config = {
-	entry: "./src/js/app/index.js",
+	entry: './src/js/app/index.js',
 	externals,
 	output: {
-		filename: "assets/js/app.js",
+		filename: 'assets/js/app.js',
 		path: __dirname,
-		library: ["ang", "[name]"],
-		libraryTarget: "this"
+		library: [ 'ang', '[name]' ],
+		libraryTarget: 'this',
 	},
 	resolve: {
-		modules: [__dirname, "node_modules"]
+		modules: [ __dirname, 'node_modules' ],
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: "babel-loader"
-			}
-		]
+				use: 'babel-loader',
+			},
+		],
 	},
-	plugins: [new CleanWebpackPlugin(["build"]), new WebpackRTLPlugin()],
+	plugins: [ new CleanWebpackPlugin( [ 'build' ] ), new WebpackRTLPlugin() ],
 	stats: {
-		children: false
+		children: false,
 	},
-	devtool: !inProduction ? "source-map" : "(none)"
+	devtool: ! inProduction ? 'source-map' : '(none)',
 };
 
 // For Productions
-if (inProduction) {
-	config.plugins.push(new webpack.optimize.UglifyJsPlugin({ sourceMap: true }));
-	config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+if ( inProduction ) {
+	config.plugins.push( new webpack.optimize.UglifyJsPlugin( { sourceMap: true } ) );
+	config.plugins.push( new webpack.LoaderOptionsPlugin( { minimize: true } ) );
 }
 
 module.exports = config;
