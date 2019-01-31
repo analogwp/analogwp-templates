231546e5a1a49bb581ca20bde718c25309903cb0
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-01-31 18:58:54 +0530

    Added Options class for Analog admin options management

---
+++ inc/class-options.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Options management.
+ *
+ * @package AnalogWP
+ */
+
+namespace Analog;
+
+/**
+ * AnalogWP options registration and management.
+ */
+class Options extends Base {
+	const OPTION_KEY = 'ang_options';
+
+	/**
+	 * Get a single option, or all if no key is provided.
+	 *
+	 * @param string|bool $key Option key.
+	 * @return array|string|bool
+	 */
+	public function get( $key = false ) {
+		$options = get_option( self::OPTION_KEY );
+
+		if ( ! $options || ! is_array( $options ) ) {
+			$options = [];
+		}
+
+		if ( false !== $key ) {
+			return isset( $options[ $key ] ) ? $options[ $key ] : false;
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Update a single option.
+	 *
+	 * @param string $key Option key.
+	 * @param string $value Option value.
+	 * @return void
+	 */
+	public function set( $key, $value ) {
+		$options         = $this->get();
+		$options[ $key ] = $value;
+		update_option( self::OPTION_KEY, $options );
+	}
+}
