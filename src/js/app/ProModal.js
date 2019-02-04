62263090b69795a1ecb47a9db25af5b04c43e34d
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-02-04 17:46:23 +0530

    Added promodal component

---
+++ src/js/app/ProModal.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+import AnalogPro from './icons/analogpro';
+const { __ } = wp.i18n;
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	max-width: 850px;
+	margin: auto;
+
+	h3 {
+		font-size: 25px;
+		font-weight: 600;
+		color: #23282C;
+		line-height: 1.4;
+		margin-top: 0;
+	}
+
+	svg {
+		margin-bottom: 30px;
+	}
+
+	p {
+		font-size: 19px;
+		color: #6D6D6D;
+		font-weight: 500;
+	}
+
+	> div {
+		width: 50%;
+	}
+
+	> div:nth-child(2) {
+		background: #fff;
+		padding: 40px;
+		flex-basis: 40%;
+		margin-left: 30px;
+	}
+
+	.button {
+		color: #fff;
+		background: #FF7865;
+		border-radius: 0;
+		font-weight: 600;
+		font-size: 15px;
+		border: none;
+		box-shadow: none;
+		padding: 10px 20px;
+		height: auto;
+		margin-top: 30px;
+	}
+
+	ul {
+		font-size: 15px;
+		color: #6D6D6D;
+		font-weight: 500;
+	}
+	li {
+		margin-bottom: 15px;
+	}
+
+	.sticky {
+		font-size: 15px;
+		font-weight: 500;
+		color: #C2C2C2;
+		margin-top: 30px;
+		margin-bottom: 0;
+	}
+`;
+
+const ProModal = () => (
+	<Container>
+		<div>
+			<AnalogPro />
+			<h3>{ __( 'Elevate your Elementor design with Analog Pro', 'ang' ) }</h3>
+			<p>{ __( 'Step up your workflow with unlimited design resources for your Elementor-powered projects.', 'ang' ) }</p>
+			<a href="https://analogwp.com/pro/" className="button" target="_blank" rel="external noopener noreferrer">{ __( 'Learn More', 'ang' ) }</a>
+		</div>
+		<div>
+			<h3>{ __( 'Why Pro', 'ang' ) }</h3>
+			<ul>
+				<li>{ __( 'Access to all template library', 'ang' ) }</li>
+				<li>{ __( 'Templates for singles, archives, popups and more', 'ang' ) }</li>
+				<li>{ __( 'Multi-niche, human made design that makes sense', 'ang' ) }</li>
+				<li>{ __( 'Unlimited license for peace of mind', 'ang' ) }</li>
+			</ul>
+
+			<p className="sticky">{ __( '* Requires Elementor Pro', 'ang' ) }</p>
+		</div>
+	</Container>
+);
+
+export default ProModal;
