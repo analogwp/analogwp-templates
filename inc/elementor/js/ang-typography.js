6f438df7ef00b4484979a975054479c75242cccb
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-03-27 01:42:50 +0530

    Added Contextual menu to switch to Page Styles tab

--- inc/elementor/js/ang-typography.js
+++ inc/elementor/js/ang-typography.js
@@ -21,4 +21,30 @@ jQuery( document ).ready( function( $ ) {
 
 		elementor.settings.page.addChangeCallback( element, handleFonts );
 	}
+
+	elementor.hooks.addFilter( 'elements/widget/contextMenuGroups', function( groups ) {
+		const PageStyles = {
+			name: 'ang_styles',
+			actions: [
+				{
+					name: 'page_styles',
+					title: 'Page Style Settings',
+					callback: switchToStyleTab,
+				},
+			],
+		};
+
+		groups.splice( 3, 0, PageStyles );
+		groups.join();
+
+		return groups;
+	} );
+
+	function switchToStyleTab() {
+		const currentView = elementor.panel.currentView;
+
+		currentView.setPage( 'page_settings' );
+		currentView.getCurrentPageView().activateTab( 'style' );
+		currentView.getCurrentPageView().render();
+	}
 } );
