17d191b8e88585cc556b56c030a85306fc4aa21b
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-04-10 11:40:40 +0530

    #34 Display Tokens submenu only when there is atleast one post

--- inc/register-settings.php
+++ inc/register-settings.php
@@ -23,22 +23,28 @@ function register_menu() {
 		'58.6'
 	);
 
-	add_submenu_page(
-		'analogwp_templates',
-		__( 'Settings', 'ang' ),
-		__( 'Settings', 'ang' ),
-		'manage_options',
-		'analogwp_templates'
-	);
+	// Display Tokens submenu only when there is atleast one post.
+	$tokens = get_posts( 'post_type=ang_tokens&post_status=publish' );
 
-	$menu_title = __( 'Tokens', 'ang' );
-	add_submenu_page(
-		'analogwp_templates',
-		$menu_title,
-		$menu_title,
-		'manage_options',
-		'edit.php?post_type=ang_tokens'
-	);
+	if ( $tokens ) {
+		add_submenu_page(
+			'analogwp_templates',
+			__( 'Settings', 'ang' ),
+			__( 'Settings', 'ang' ),
+			'manage_options',
+			'analogwp_templates'
+		);
+
+		$menu_title = __( 'Tokens', 'ang' );
+		add_submenu_page(
+			'analogwp_templates',
+			$menu_title,
+			$menu_title,
+			'manage_options',
+			'edit.php?post_type=ang_tokens'
+		);
+	}
+
 }
 
 add_action( 'admin_menu', 'Analog\settings\register_menu' );
