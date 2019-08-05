799e241ef5370cc0dfa3a7fd7d6e75f6f12f393f
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-08-05 17:15:20 +0530

    #84 Redo color controls based on feedback

--- inc/elementor/class-colors.php
+++ inc/elementor/class-colors.php
@@ -131,38 +131,76 @@ class Colors extends Module {
 			]
 		);
 
-		for ( $i = 1; $i <= 8; $i++ ) {
-			$element->add_control(
-				'ang_color_toggle' . $i,
-				[
-					/* translators: %s: Color Index. */
-					'label'        => sprintf( __( 'Color %s', 'ang' ), $i ),
-					'type'         => Controls_Manager::POPOVER_TOGGLE,
-					'return_value' => 'yes',
-				]
-			);
+		$element->add_control(
+			'ang_color_accent_primary',
+			[
+				'label'       => __( 'Primary Accent', 'ang' ),
+				'type'        => Controls_Manager::COLOR,
+				'description' => __( 'Applies to all links by default', 'ang' ),
+				'selectors'   => [
+					'{{WRAPPER}} a:not(.button):not(.elementor-button)' => 'color: {{VALUE}}',
+				],
+			]
+		);
 
-			$element->start_popover();
+		$element->add_control(
+			'ang_color_accent_secondary',
+			[
+				'label'       => __( 'Secondary Accent', 'ang' ),
+				'type'        => Controls_Manager::COLOR,
+				'description' => __( 'Adds a background color to all buttons on page.', 'ang' ),
+				'selectors'   => [
+					'{{WRAPPER}} .elementor-button, {{WRAPPER}} .button, {{WRAPPER}} button' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
 
-			$element->add_control(
-				'ang_color_label_' . $i,
-				[
-					'label'   => __( 'Label', 'ang' ),
-					'default' => 'Color ' . $i,
-					'type'    => Controls_Manager::TEXT,
-				]
-			);
+		$element->add_control(
+			'ang_color_text_headings_dark',
+			[
+				'label'       => __( 'Text and Headings (Dark)', 'ang' ),
+				'type'        => Controls_Manager::COLOR,
+				'description' => __( 'Applies on the text and titles over light bg', 'ang' ),
+				'selectors'   => [
+					'{{WRAPPER}},{{WRAPPER}} h1, {{WRAPPER}} h2, {{WRAPPER}} h3, {{WRAPPER}} h4, {{WRAPPER}} h5, {{WRAPPER}} h6' => 'color: {{VALUE}}',
+					':root' => '--ang_color_text_headings_dark: {{VALUE}}',
+				],
+			]
+		);
 
-			$element->add_control(
-				'ang_color_' . $i,
-				[
-					'label' => __( 'Color', 'ang' ),
-					'type'  => Controls_Manager::COLOR,
-				]
-			);
+		$element->add_control(
+			'ang_color_text_headings_light',
+			[
+				'label'       => __( 'Text and Headings (Light)', 'ang' ),
+				'type'        => Controls_Manager::COLOR,
+				'description' => __( 'Applies on the text and titles over dark bg', 'ang' ),
+				'selectors'   => [
+					':root' => '--ang_color_text_headings_light: {{VALUE}}',
+				],
+			]
+		);
 
-			$element->end_popover();
-		}
+		$element->add_control(
+			'ang_color_background_dark',
+			[
+				'label'     => __( 'Dark Background', 'ang' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .dark-bg' => 'background-color: {{VALUE}}; color: var(--ang_color_text_headings_light)',
+				],
+			]
+		);
+
+		$element->add_control(
+			'ang_color_background_light',
+			[
+				'label'     => __( 'Light Background', 'ang' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .light-bg' => 'background-color: {{VALUE}}; color: var(--ang_color_text_headings_dark)',
+				],
+			]
+		);
 
 		$element->end_controls_section();
 	}
