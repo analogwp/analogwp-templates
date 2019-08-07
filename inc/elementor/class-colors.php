4c514944fb6153cf39b5f23e01efc5162c54f2e6
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-08-07 14:01:09 +0530

    #84 Make control descriptions full width

--- inc/elementor/class-colors.php
+++ inc/elementor/class-colors.php
@@ -38,6 +38,7 @@ class Colors extends Module {
 				'label'       => __( 'Primary Accent', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Sets the primary brand color, applies on Links.', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					'{{WRAPPER}} a:not(.button):not(.elementor-button), {{WRAPPER}} .sk-accent-1' => 'color: {{VALUE}}',
 				],
@@ -50,6 +51,7 @@ class Colors extends Module {
 				'label'       => __( 'Secondary Accent', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Sets the default color for Buttons. You can also apply button colors in the Global buttons tab.', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					'{{WRAPPER}} .elementor-button, {{WRAPPER}} .button, {{WRAPPER}} button, {{WRAPPER}} .sk-accent-2' => 'background-color: {{VALUE}}',
 				],
@@ -62,6 +64,7 @@ class Colors extends Module {
 				'label'       => __( 'Text over light background', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Applies on the text and titles over a section or column with light bg', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					'{{WRAPPER}},{{WRAPPER}} h1, {{WRAPPER}} h2, {{WRAPPER}} h3, {{WRAPPER}} h4, {{WRAPPER}} h5, {{WRAPPER}} h6' => 'color: {{VALUE}}',
 					':root, {{WRAPPER}} .sk-text-light' => '--ang_color_text_light: {{VALUE}}',
@@ -75,6 +78,7 @@ class Colors extends Module {
 				'label'       => __( 'Text over dark background', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Applies on the text and titles over a section or column with dark bg', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					':root, {{WRAPPER}} .sk-text-dark' => '--ang_color_text_dark: {{VALUE}}',
 				],
@@ -87,6 +91,7 @@ class Colors extends Module {
 				'label'       => __( 'Light Background', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Apply the class <strong>sk-light-bg</strong> to a section or column to apply this color as a background. Text will inherit the <strong>Text over Light bg</strong> color.', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					'{{WRAPPER}} .sk-light-bg' => 'background-color: {{VALUE}}; color: var(--ang_color_text_light)',
 				],
@@ -99,6 +104,7 @@ class Colors extends Module {
 				'label'       => __( 'Dark Background', 'ang' ),
 				'type'        => Controls_Manager::COLOR,
 				'description' => __( 'Apply the class <strong>sk-dark-bg</strong> to a section or column to apply this color as a background. Text will inherit the <strong>Text over Dark bg</strong> color.', 'ang' ),
+				'classes'     => 'ang-description-wide',
 				'selectors'   => [
 					'{{WRAPPER}} .sk-dark-bg'   => 'background-color: {{VALUE}}; color: var(--ang_color_text_dark)',
 					'{{WRAPPER}} .sk-dark-bg h1,
