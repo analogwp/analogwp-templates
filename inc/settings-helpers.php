39734aefa3ef95c0c8d6b134ab186aadf97bf28d
Krishna Kant <lushkant@gmail.com>
2019-10-01 15:51:08 +0530

    #178, Add Setting Options Getters & Setters Functions

--- inc/settings-helpers.php
+++ inc/settings-helpers.php
@@ -14,3 +14,50 @@ function ang_clean( $var ) {
 		return is_scalar( $var ) ? sanitize_text_field( $var ) : $var;
 	}
 }
+
+/**
+ * Output admin fields.
+ *
+ * Loops though the Analog options array and outputs each field.
+ *
+ * @param array $options Opens array to output.
+ */
+function ang_admin_fields( $options ) {
+
+	if ( ! class_exists( 'Admin_Settings', false ) ) {
+		include dirname( __FILE__ ) . '/class-admin-settings.php';
+	}
+
+	Admin_Settings::output_fields( $options );
+}
+
+/**
+ * Update all settings which are passed.
+ *
+ * @param array $options Option fields to save.
+ * @param array $data Passed data.
+ */
+function ang_update_options( $options, $data = null ) {
+
+	if ( ! class_exists( 'Admin_Settings', false ) ) {
+		include dirname( __FILE__ ) . '/class-admin-settings.php';
+	}
+
+	Admin_Settings::save_fields( $options, $data );
+}
+
+/**
+ * Get a setting from the settings API.
+ *
+ * @param mixed $option_name Option name to save.
+ * @param mixed $default Default value to save.
+ * @return string
+ */
+function ang_settings_get_option( $option_name, $default = '' ) {
+
+	if ( ! class_exists( 'Admin_Settings', false ) ) {
+		include dirname( __FILE__ ) . '/class-admin-settings.php';
+	}
+
+	return Admin_Settings::get_option( $option_name, $default );
+}
