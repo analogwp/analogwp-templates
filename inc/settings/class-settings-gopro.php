804a83a17ac131cc526255a76c10c969e754887a
Krishna Kant <lushkant@gmail.com>
2019-10-03 13:45:51 +0530

    #178, Add Go Pro promotional Tab at Settings Page

---
+++ inc/settings/class-settings-gopro.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Analog Go Pro Promotional Tab.
+ *
+ * @package Analog/Admin
+ */
+
+namespace Analog\settings;
+
+defined( 'ABSPATH' ) || exit;
+
+if ( class_exists( 'Settings_GoPro', false ) ) {
+	return new Settings_GoPro();
+}
+
+/**
+ * Admin_Settings_GoPro.
+ */
+class Settings_GoPro extends Settings_Page {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->id    = 'gopro';
+		$this->label = __( 'Go Pro', 'ang' );
+
+		parent::__construct();
+	}
+
+	/**
+	 * Get settings array.
+	 *
+	 * @return array
+	 */
+	public function get_settings() {
+
+		$settings = apply_filters(
+			'ang_gopro_settings',
+			array(
+				array(
+					'title' => __( 'Access an interconnected library of Template Kits, blocks and additional design control with Style kits Pro.', 'ang' ),
+					'type'  => 'title',
+					'id'    => 'ang_gopro_title',
+				),
+				array(
+					'to'       => '',
+					'id'       => 'ang_gopro_button',
+					'type'     => 'button',
+					'class'    => 'ang-gopro-button ang-button',
+					'value'    => __( 'Explore Style Kits Pro', 'ang' ),
+				),
+				array(
+					'type' => 'sectionend',
+					'id'   => 'ang_gopro_title',
+				),
+			)
+		);
+
+		return apply_filters( 'ang_get_settings_' . $this->id, $settings );
+	}
+
+	/**
+	 * Output the settings.
+	 */
+	public function output() {
+		$settings = $this->get_settings();
+
+		Admin_Settings::output_fields( $settings );
+	}
+
+	/**
+	 * Save settings.
+	 */
+	public function save() {
+		$settings = $this->get_settings();
+
+		Admin_Settings::save_fields( $settings );
+	}
+}
+
+return new Settings_GoPro();
