8f9b424371b145816672491177ba4a688de2281b
Krishna Kant <lushkant@gmail.com>
2019-10-03 16:47:08 +0530

    #178, Add Go Pro Tab View for Html output

--- inc/settings/class-settings-gopro.php
+++ inc/settings/class-settings-gopro.php
@@ -24,8 +24,9 @@ class Settings_GoPro extends Settings_Page {
 	public function __construct() {
 		$this->id    = 'gopro';
 		$this->label = __( 'Go Pro', 'ang' );
-
 		parent::__construct();
+
+		add_action( 'ang_settings_tabs_' . $this->id, [ $this, 'get_pro' ] );
 	}
 
 	/**
@@ -34,30 +35,37 @@ class Settings_GoPro extends Settings_Page {
 	 * @return array
 	 */
 	public function get_settings() {
-
 		$settings = apply_filters(
 			'ang_gopro_settings',
 			array(
 				array(
-					'title' => __( 'Access an interconnected library of Template Kits, blocks and additional design control with Style kits Pro.', 'ang' ),
-					'type'  => 'title',
-					'id'    => 'ang_gopro_title',
+				'title' => __( 'Access an interconnected library of Template Kits, blocks and additional design control with Style kits Pro.', 'ang' ),
+				'type'  => 'title',
+				'id'    => 'ang_gopro_title',
 				),
 				array(
-					'to'       => '',
-					'id'       => 'ang_gopro_button',
-					'type'     => 'button',
-					'class'    => 'ang-gopro-button ang-button',
-					'value'    => __( 'Explore Style Kits Pro', 'ang' ),
+				'to'    => '',
+				'id'    => 'ang_gopro_button',
+				'type'  => 'button',
+				'class' => 'ang-gopro-button ang-button',
+				'value' => __( 'Explore Style Kits Pro', 'ang' ),
 				),
 				array(
-					'type' => 'sectionend',
-					'id'   => 'ang_gopro_title',
+				'type' => 'sectionend',
+				'id'   => 'ang_gopro_title',
 				),
 			)
 		);
 
 		return apply_filters( 'ang_get_settings_' . $this->id, $settings );
+	}
+
+	/**
+	 * Get Pro Tab Data.
+	 */
+	public function get_pro() {
+		echo 'hello';
+		include dirname( __FILE__ ) . '/views/html-admin-settings-gopro.php';
 	}
 
 	/**
---
+++ inc/settings/views/html-admin-settings-gopro.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Hello';
+?>
+
+<div>
+<h1>Hello</h1>
+</div>
+
