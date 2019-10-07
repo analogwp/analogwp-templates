55f7f749dde1535b3317d2bcee9cc10caee39ef1
Krishna Kant <lushkant@gmail.com>
2019-10-07 13:53:40 +0530

    #178, Update Newsletter field to have current user email by default

--- inc/settings/views/html-admin-settings.php
+++ inc/settings/views/html-admin-settings.php
@@ -13,6 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $tab_exists        = isset( $tabs[ $current_tab ] ) || has_action( 'ang_sections_' . $current_tab ) || has_action( 'ang_settings_' . $current_tab ) || has_action( 'ang_settings_tabs_' . $current_tab );
 $current_tab_label = isset( $tabs[ $current_tab ] ) ? $tabs[ $current_tab ] : '';
+$current_user      = wp_get_current_user();
 
 if ( ! $tab_exists ) {
 	wp_safe_redirect( admin_url( 'admin.php?page=ang-settings' ) );
@@ -23,7 +24,7 @@ if ( 'gopro' === $current_tab ) {
 	$background_img = 'background-image:url(' . esc_url( ANG_PLUGIN_URL . 'assets/img/gopro_frames.png' ) . ');';
 }
 ?>
-<div class="wrap ang <?php echo esc_attr( $current_tab );?>">
+<div class="wrap ang <?php echo esc_attr( $current_tab ); ?>">
 	<h1 class="menu-title"><?php esc_html_e( 'Style Kits Settings', 'pulse' ); ?></h1>
 	<div class="ang-wrapper">
 		<form method="<?php echo esc_attr( apply_filters( 'ang_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data" style="<?php echo esc_attr( $background_img ); ?>">
@@ -70,8 +71,8 @@ if ( 'gopro' === $current_tab ) {
 				<h1><?php esc_html_e( 'Sign up for updates', 'ang' ); ?></h1>
 				<p><?php esc_html_e( 'Sign up to Analog Newsletter and get notified about product updates, freebies and more.', 'ang' ); ?></p>
 				<form id="ang-newsletter" action="" class="form-newsletter">
-					<input id="ang-newsletter-email" type="email" placeholder="Enter your email" />
-					<button id="ang-newsletter-submit" class="ang-button" type="submit"><?php esc_html_e( 'Subscribe up to newsletter', 'ang'); ?></button>
+					<input id="ang-newsletter-email" type="email" placeholder="Enter your email" value="<?php echo esc_attr( $current_user->user_email ); ?>"/>
+					<button id="ang-newsletter-submit" class="ang-button" type="submit"><?php esc_html_e( 'Subscribe up to newsletter', 'ang' ); ?></button>
 				</form>
 				<p><?php esc_html_e( 'By signing up you agree to our ', 'ang' ); ?><a href="<?php echo esc_url( 'https://analogwp.com/privacy-policy/' ); ?>" target="_blank"><?php esc_html_e( 'privacy and terms' ); ?><span class="dashicons dashicons-external"></span></a></p>
 			</div>
