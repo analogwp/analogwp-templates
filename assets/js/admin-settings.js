5a1e61a1ffeb8d18891663947e7b62cce63f54ab
Krishna Kant <lushkant@gmail.com>
2019-10-09 13:53:41 +0530

    Remove colorpicker handlers

--- assets/js/admin-settings.js
+++ assets/js/admin-settings.js
@@ -54,53 +54,6 @@
 		}
 		$( '#ang_rollback_version_button' ).on( 'click', processPluginRollback );
 
-		// Color picker
-		$( '.colorpick' )
-			.iris( {
-				change: function( event, ui ) {
-					$( this )
-						.parent()
-						.find( '.colorpickpreview' )
-						.css( { backgroundColor: ui.color.toString() } );
-				},
-				hide: true,
-				border: true,
-			} )
-
-			.on( 'click focus', function( event ) {
-				event.stopPropagation();
-				$( '.iris-picker' ).hide();
-				$( this )
-					.closest( 'td' )
-					.find( '.iris-picker' )
-					.show();
-				$( this ).data( 'original-value', $( this ).val() );
-			} )
-
-			.on( 'change', function() {
-				if ( $( this ).is( '.iris-error' ) ) {
-					const original_value = $( this ).data( 'original-value' );
-
-					if (
-						original_value.match(
-							/^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/
-						)
-					) {
-						$( this )
-							.val( $( this ).data( 'original-value' ) )
-							.change();
-					} else {
-						$( this )
-							.val( '' )
-							.change();
-					}
-				}
-			} );
-
-		$( 'body' ).on( 'click', function() {
-			$( '.iris-picker' ).hide();
-		} );
-
 		// Edit prompt
 		$( function() {
 			let changed = false;
