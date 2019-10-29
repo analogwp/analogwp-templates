3f993f9c081cafdc201fc309e5c32e65c04d2be5
Ram Ratan Maurya <ratan.maurya@gmail.com>
2019-10-29 12:55:22 +0530

    Hide Extensions tab if no settings are added

--- inc/settings/class-settings-page.php
+++ inc/settings/class-settings-page.php
@@ -71,6 +71,10 @@ abstract class Settings_Page {
 	 * @return mixed
 	 */
 	public function add_settings_page( $pages ) {
+		if ( 'extensions' === $this->id && ! has_filter( 'ang_get_settings_' . $this->id ) ) {
+			return $pages;
+		}
+
 		$pages[ $this->id ] = $this->label;
 
 		return $pages;
