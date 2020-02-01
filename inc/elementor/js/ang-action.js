ae78883b857bf412c4f0caff18f04b60d4e357c3
Ram Ratan Maurya <ratan.maurya@gmail.com>
2020-02-01 15:57:36 +0530

    #281 Make ang-action control actions extendable

--- inc/elementor/js/ang-action.js
+++ inc/elementor/js/ang-action.js
@@ -377,13 +377,19 @@ jQuery( window ).on( 'elementor:init', function() {
 			return events;
 		},
 
-		performAction: function( name ) {
+		actions: function() {
 			const actions = {
 				export_css: 'handleCSSExport',
 				reset_css: 'handleCSSReset',
 				save_token: 'handleSaveToken',
+				update_token: 'handleTokenUpdate',
 			};
 
+			return actions;
+		},
+
+		performAction: function( name ) {
+			const actions = this.actions();
 			return this[ actions[ name ] ]();
 		},
 
