09f12391b29359d51125820502bc9486295c6355
Krishna Kant <lushkant@gmail.com>
2020-02-25 13:48:20 +0530

    Fix translation hook

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -64,7 +64,7 @@ final class Plugin {
 	 * @since 1.6.0
 	 */
 	public function register() {
-		add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
+		add_action( 'init', array( self::$instance, 'load_textdomain' ) );
 		add_filter( 'plugin_action_links_' . plugin_basename( ANG_PLUGIN_FILE ), array( self::$instance, 'plugin_action_links' ) );
 		add_action( 'admin_enqueue_scripts', array( self::$instance, 'scripts' ) );
 		add_filter( 'analog/app/strings', array( self::$instance, 'send_strings_to_app' ) );
