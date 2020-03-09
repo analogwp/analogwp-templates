ec4e0fbba23ffb7cd5e56138e2af63d4e9ef77bd
Ram Ratan Maurya <ratan.maurya@gmail.com>
2020-03-09 16:47:36 +0530

    Re-enable SK row actions as Kits can now be edited

--- inc/elementor/kit/Kits_List_Table.php
+++ inc/elementor/kit/Kits_List_Table.php
@@ -2,6 +2,7 @@
 
 namespace Analog\Elementor\Kit;
 
+use Analog\Plugin;
 use Analog\Utils;
 use Elementor\Core\Base\Document;
 use Elementor\TemplateLibrary\Source_Local;
@@ -116,13 +117,13 @@ class Kits_List_Table extends \WP_List_Table {
 	 * @return string
 	 */
 	public function column_title( $item ) {
-		// $document = Plugin::$instance->documents->get( $item['id'] );
+		$document = Plugin::elementor()->documents->get( $item['id'] );
 		$edit_url = get_edit_post_link( $item['id'] );
 
 		$output = '<strong>';
 
 		/* translators: %s: Kit Title */
-		$output .= '<a class="row-title" style="pointer-events:none;" href="' . esc_url( $edit_url ) . '" aria-label="' . sprintf( __( '%s (Edit)', 'ang' ), $item['title'] ) . '">' . esc_html( $item['title'] ) . '</a>';
+		$output .= '<a class="row-title" href="' . esc_url( $edit_url ) . '" aria-label="' . sprintf( __( '%s (Edit)', 'ang' ), $item['title'] ) . '">' . esc_html( $item['title'] ) . '</a>';
 		if ( (int) get_option( \Elementor\Core\Kits\Manager::OPTION_ACTIVE ) === $item['id'] ) {
 			$output .= '&nbsp;&mdash;	&nbsp;<span class="post-state"><span style="color:#32b644;">&#9679; ' . esc_html__( 'Global Style Kit', 'ang' ) . '</span></span>';
 		}
@@ -131,10 +132,10 @@ class Kits_List_Table extends \WP_List_Table {
 
 		// Get actions.
 		$actions = array(
-			// 'edit'            => '<a href="' . esc_url( $edit_url ) . '">' . __( 'Edit', 'ang' ) . '</a>',
-			'trash'           => '<a href="' . esc_url( get_delete_post_link( $item['id'] ) ) . '" class="submitdelete">' . __( 'Trash', 'ang' ) . '</a>',
-			'export-template' => '<a href="' . esc_url( $this->get_export_link( $item['id'] ) ) . '">' . __( 'Export Theme Style Kit', 'ang' ) . '</a>',
-		// 'edit_with_elementor' => '<a href="' . esc_url( $document->get_edit_url() ) . '">' . __( 'Edit with Elementor', 'ang' ) . '</a>',
+			'edit'                => '<a href="' . esc_url( $edit_url ) . '">' . __( 'Edit', 'ang' ) . '</a>',
+			'trash'               => '<a href="' . esc_url( get_delete_post_link( $item['id'] ) ) . '" class="submitdelete">' . __( 'Trash', 'ang' ) . '</a>',
+			'export-template'     => '<a href="' . esc_url( $this->get_export_link( $item['id'] ) ) . '">' . __( 'Export Theme Style Kit', 'ang' ) . '</a>',
+			'edit_with_elementor' => '<a href="' . esc_url( $document->get_edit_url() ) . '">' . __( 'Edit with Elementor', 'ang' ) . '</a>',
 		);
 
 		$row_actions = array();
