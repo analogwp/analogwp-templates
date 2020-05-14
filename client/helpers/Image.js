0f503b12d24c43e16c1e40455c33e22c81cc37a0
Ram Ratan Maurya <ratan.maurya@gmail.com>
2020-05-14 09:22:12 +0530

    Update Waypoint package to latest version
    Fixed a warning with outdated React function call

--- client/helpers/Image.js
+++ client/helpers/Image.js
@@ -1,6 +1,6 @@
 import { Waypoint } from 'react-waypoint';
 
-const Image = ( { template, ...other } ) => {
+const Image = ( { template, ...attributes } ) => {
 	const { thumbnail, title } = template;
 	const [ image, setImage ] = React.useState( AGWP.pluginURL + 'assets/img/placeholder.svg' );
 
@@ -8,7 +8,7 @@ const Image = ( { template, ...other } ) => {
 		<Waypoint onEnter={ () => {
 			setImage( thumbnail );
 		} }>
-			<img src={ image } loading="lazy" width="280" height="390" alt={ title } { ...other } />
+			<img src={ image } loading="lazy" width="280" height="390" alt={ title } { ...attributes } />
 		</Waypoint>
 	);
 };
