3730d60bb0e31b8b6b4c3c837313d50f98f7ed86
gvgvgvijayan <gvgvgvijayan@gmail.com>
2020-07-02 16:28:33 +0530

    #371 fixed merge conflict in utils class

--- inc/Utils.php
+++ inc/Utils.php
@@ -796,6 +796,27 @@ class Utils extends Base {
 
 		return $tab;
 	}
+
+	/**
+	 * Filters upload files path and URL
+	 *
+	 * This static method is responsible to filter and modify the uploaded file path to plugin defined constant path.
+	 *
+	 * @since 1.6.10
+	 *
+	 * @param Array $arr An associative array which holds upload path configuration.
+	 *
+	 * @return Array
+	 */
+	public static function style_kits_upload_dir( $arr ) {
+		$folder = ANG_SK_UPLOAD_DIR;
+
+		$arr['path']   = $arr['basedir'] . $folder;
+		$arr['url']    = $arr['baseurl'] . $folder;
+		$arr['subdir'] = $folder;
+
+		return $arr;
+	}
 }
 
 new Utils();
