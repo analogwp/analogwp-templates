bd7a99e74629f1310c693f2978815b3b5102fbbe
Krishna Kant <lushkant@gmail.com>
2020-07-16 22:24:37 +0530

    #393 Improve box-shadow in Blocks list grid

--- client/blocks-library/BlocksList.js
+++ client/blocks-library/BlocksList.js
@@ -27,7 +27,7 @@ const Container = styled.div`
 
 		> div {
 			background: #fff;
-			box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.12);
+			box-shadow: 0px 5px 20px rgba(0,0,0,0.05);
 			position: relative;
 			margin-bottom: 25px;
 		}
