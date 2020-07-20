57ee1fbff2f0779f2d6c191f10a64dca0777de28
gvgvgvijayan <gvgvgvijayan@gmail.com>
2020-07-20 18:01:20 +0530

    #394 Modified logic to check is elementor loaded

--- analogwp-templates.php
+++ analogwp-templates.php
@@ -127,10 +127,10 @@ function analog_fail_load() {
 		require_once ABSPATH . 'wp-admin/includes/plugin.php';
 	}
 	$file_path         = 'elementor/elementor.php';
-	$installed_plugins = get_plugins();
-	$elementor         = isset( $installed_plugins[ $file_path ] );
-
-	if ( $elementor ) {
+		
+	$is_elementor_loaded = in_array($file_path, apply_filters('active_plugins', get_option('active_plugins')));
+	
+	if ( !$is_elementor_loaded ) {
 		if ( ! current_user_can( 'activate_plugins' ) ) {
 			return;
 		}
