81be4ad0d0170aa09bd3a2b6e5e815b1f99ac74a
Ram Ratan Maurya <ratan.maurya@gmail.com>
2020-07-23 09:36:32 +0530

    Add Separate Style Kits tab in Global panel

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -248,6 +248,7 @@ final class Plugin {
 		require_once ANG_PLUGIN_DIR . 'inc/elementor/kit/Manager.php';
 		require_once ANG_PLUGIN_DIR . 'inc/elementor/kit/Kits_List_Table.php';
 		require_once ANG_PLUGIN_DIR . 'inc/Core/Util/Migration.php';
+		require_once ANG_PLUGIN_DIR . 'inc/elementor/kit/tabs/Theme_Style_Kits.php';
 
 		if ( ! defined( 'ANG_PRO_VERSION' ) ) {
 			require_once ANG_PLUGIN_DIR . 'inc/elementor/Promotions.php';
