27bcc950f919055e0d58a8fb005fd19bb7d982d8
Krishna Kant <lushkant@gmail.com>
2022-06-17 21:50:46 +0530

    Add fallback to Global kit for when a page/kit doesn’t exist

--- inc/elementor/globals/class-colors.php
+++ inc/elementor/globals/class-colors.php
@@ -16,26 +16,32 @@ class Colors extends Base {
 	}
 
 	protected function get_kit_items() {
-		$result = array();
-		$kit    = Plugin::elementor()->kits_manager->get_active_kit_for_frontend();
+		$result     = array();
+		$global_kit = Plugin::elementor()->kits_manager->get_active_kit_for_frontend();
 
-		$system_items = $kit->get_settings_for_display( 'system_colors' );
-		$custom_items = $kit->get_settings_for_display( 'custom_colors' );
+		$system_items = $global_kit->get_settings_for_display( 'system_colors' );
+		$custom_items = $global_kit->get_settings_for_display( 'custom_colors' );
 
 		if ( ! $system_items ) {
-			$system_items = [];
+			$system_items = array();
 		}
 
 		if ( ! $custom_items ) {
-			$custom_items = [];
+			$custom_items = array();
 		}
 
 		$items = array_merge( $system_items, $custom_items );
 
 		// Custom hack for getting the active kit on page.
 		$current_page_id = Options::get_instance()->get( 'ang_current_page_id' );
+		$kit             = false;
 		if ( $current_page_id ) {
 			$kit = Utils::get_document_kit( $current_page_id );
+		}
+
+		// Fallback to global kit.
+		if ( ! $kit ) {
+			$kit = $global_kit;
 		}
 
 		$color_keys = array(
