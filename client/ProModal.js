70af0a8a0ed1ea26f7804b56d8ae507c98224361
Krishna Kant <lushkant@gmail.com>
2022-09-09 23:39:21 +0530

    Update pro modal alert

--- client/ProModal.js
+++ client/ProModal.js
@@ -4,7 +4,7 @@ const { __ } = wp.i18n;
 const ProModal = ( { type } ) => (
 	<div className="pro-modal-container">
 		<p>
-			<span role="img" aria-label="zap">⚡</span> { __( 'To import Pro ', 'ang' ) + type + __( ', you’ll need an active ', 'ang' ) } <strong>{ __( 'Style Kits Pro', 'ang' ) }</strong> { __( 'license.', 'ang' ) }
+			<span role="img" aria-label="zap">⚡</span> { __( 'Get unlimited access to all library and features with Style Kits PRO.', 'ang' ) }
 		</p>
 		<Button isSecondary ><ExternalLink href="https://analogwp.com/style-kits-pro/?utm_medium=plugin&utm_source=library&utm_campaign=style+kits+pro">{ __( 'Learn More', 'ang' ) }</ExternalLink></Button>
 	</div>
