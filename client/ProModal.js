9264f9a6c8adf02ca460cca0d5b2fc7d92930244
Krishna Kant <lushkant@gmail.com>
2022-09-22 13:18:34 +0530

    Improve Pro notice
    #578

--- client/ProModal.js
+++ client/ProModal.js
@@ -3,9 +3,7 @@ const { __ } = wp.i18n;
 
 const ProModal = ( { type } ) => (
 	<div className="pro-modal-container">
-		<p>
-			<span role="img" aria-label="zap">⚡</span> { __( 'Get unlimited access to all library and features with Style Kits PRO.', 'ang' ) }
-		</p>
+		<p>{ __( 'Get unlimited access to all library and features with Style Kits PRO.', 'ang' ) }</p>
 		<Button isSecondary ><ExternalLink href="https://analogwp.com/style-kits-pro/?utm_medium=plugin&utm_source=library&utm_campaign=style+kits+pro">{ __( 'Learn More', 'ang' ) }</ExternalLink></Button>
 	</div>
 );
