d7c777a93273907a42b656243be68e7e3ef6e789
Krishna Kant <lushkant@gmail.com>
2022-09-23 22:19:36 +0530

    Improve options class usage

--- inc/elementor/class-ang-action.php
+++ inc/elementor/class-ang-action.php
@@ -98,7 +98,7 @@ class ANG_Action extends Base_Data_Control {
 			array(
 				'saveToken'       => rest_url( 'agwp/v1/tokens/save' ),
 				'cssDir'          => \Elementor\Core\Files\Base::get_base_uploads_url() . \Elementor\Core\Files\Base::DEFAULT_FILES_DIR,
-				'globalKit'       => Options::get_instance()->get( 'global_kit' ),
+				'globalKit'       => $options->get( 'global_kit' ),
 				'translate'       => array(
 					'resetMessage'                 => __( 'This will clean-up all the values from the current Theme Style kit. If you need to revert, you can do so at the Revisions tab.', 'ang' ),
 					'resetHeader'                  => __( 'Are you sure?', 'ang' ),
