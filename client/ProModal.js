c3b4f2ca01937514a5e16b5e5235caf9cded2115
Krishna Kant <lushkant@gmail.com>
2022-10-06 12:15:37 +0530

    Remove external link icon
    closes #581

--- client/ProModal.js
+++ client/ProModal.js
@@ -4,7 +4,7 @@ const { __ } = wp.i18n;
 const ProModal = () => (
 	<div className="pro-modal-container">
 		<p>{ __( 'Get unlimited access to the Style Kits library and features with the PRO version.', 'ang' ) }</p>
-		<ExternalLink href="https://analogwp.com/pricing/?utm_medium=plugin&utm_source=library&utm_campaign=style+kits+pro">{ __( 'View Plans', 'ang' ) }</ExternalLink>
+		<a href="https://analogwp.com/pricing/?utm_medium=plugin&utm_source=library&utm_campaign=style+kits+pro" target="_blank">{ __( 'View Plans', 'ang' ) }</a>
 	</div>
 );
 
