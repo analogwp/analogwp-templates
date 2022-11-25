60c9458cd8e423c4ae36ebee0402f9253c1fedad
Krishna Kant <lushkant@gmail.com>
2022-11-25 20:21:09 +0530

    Run options update only once

--- inc/class-onboarding.php
+++ inc/class-onboarding.php
@@ -174,9 +174,13 @@ class Onboarding {
 	 * @return void
 	 */
 	public function render_markup() {
+		$ran_onboarding = get_option( 'ran_onboarding' );
+
 		// Checks off onboarding if the user has been through it.
 		// Uses global options table as onboarding runs even without all our files active.
-		update_option( 'ran_onboarding', true );
+		if ( ! $ran_onboarding ) {
+			update_option( 'ran_onboarding', true );
+		}
 
 		// Enqueue assets.
 		$this->enqueue_assets();
