4c5cd62aa8fccce4feed11b754ee416ace9e1c0d
Krishna Kant <lushkant@gmail.com>
2023-04-04 13:42:05 +0530

    Update to new Root render method of React 18

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
 import App from './App';
 
-const { render } = wp.element;
+const { createRoot } = wp.element;
+
+const containerElementID = 'analogwp-templates';
 
 const waitForEl = ( selector, callback ) => {
-	if ( ! document.getElementById( 'analogwp-templates' ) ) {
+	if ( ! document.getElementById( containerElementID ) ) {
 		setTimeout( function() {
 			window.requestAnimationFrame( function() {
 				waitForEl( selector, callback );
@@ -15,6 +17,7 @@ const waitForEl = ( selector, callback ) => {
 };
 
 // We don't use a variable here because in Elementor modal the element is added dynamically.
-waitForEl( document.getElementById( 'analogwp-templates' ), () => {
-	render( <App />, document.getElementById( 'analogwp-templates' ) );
+waitForEl( document.getElementById( containerElementID ), () => {
+	const containerRoot = createRoot( document.getElementById( containerElementID ) );
+	containerRoot.render( <App /> );
 } );
