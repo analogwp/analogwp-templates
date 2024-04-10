604fc9eba32d7504b1411dfcf1296090c1191ee8
Krishna Kant Chourasiya <lushkant@gmail.com>
2024-04-10 22:34:46 +0530

    Update Elementor compat version numbers

--- analogwp-templates.php
+++ analogwp-templates.php
@@ -16,8 +16,8 @@
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ang
- * Elementor tested up to: 3.20.3
- * Elementor Pro tested up to: 3.20.2
+ * Elementor tested up to: 3.20.4
+ * Elementor Pro tested up to: 3.20.3
  */
 
 defined( 'ABSPATH' ) || exit;
