35635c6a469bbd4b8ae7f2057ba6c1a625a882d7
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-11-12 19:10:06 +0530

    Update app build file enqueue

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -96,7 +96,7 @@ final class Plugin {
 
 		wp_enqueue_script(
 			'analogwp-app',
-			ANG_PLUGIN_URL . 'assets/js/app.js',
+			ANG_PLUGIN_URL . 'assets/js/app/index.js',
 			array(
 				'react',
 				'react-dom',
@@ -108,7 +108,7 @@ final class Plugin {
 				'wp-api-fetch',
 				'wp-html-entities',
 			),
-			filemtime( ANG_PLUGIN_DIR . 'assets/js/app.js' ),
+			filemtime( ANG_PLUGIN_DIR . 'assets/js/app/index.js' ),
 			true
 		);
 		wp_set_script_translations( 'analogwp-app', 'ang', ANG_PLUGIN_DIR . 'languages' );
