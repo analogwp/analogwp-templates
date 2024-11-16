398f3df7b2278f701056cdc97c7cbb887ad8960f
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-11-16 21:18:50 +0530

    Add Freemius SDK

--- analogwp-templates.php
+++ analogwp-templates.php
@@ -185,6 +185,41 @@ if ( is_readable( $vendor_file ) ) {
 	require_once $vendor_file;
 }
 
+if ( ! function_exists( 'sk_fs' ) ) {
+	// Create a helper function for easy SDK access.
+	function sk_fs() {
+		global $sk_fs;
+
+		if ( ! isset( $sk_fs ) ) {
+			// Include Freemius SDK.
+			require_once dirname(__FILE__) . '/freemius/start.php';
+
+			$sk_fs = fs_dynamic_init( array(
+				'id'                  => '17032',
+				'slug'                => 'analogwp-templates',
+				'type'                => 'plugin',
+				'public_key'          => 'pk_e05579cda0ad78db31e94616185cc',
+				'is_premium'          => false,
+				'has_addons'          => false,
+				'has_paid_plans'      => false,
+				'menu'                => array(
+					'slug'           => 'analogwp_templates',
+					'first-path'     => 'admin.php?page=analog_onboarding&from=freemius',
+					'account'        => false,
+					'support'        => false,
+				),
+			) );
+		}
+
+		return $sk_fs;
+	}
+
+	// Init Freemius.
+	sk_fs();
+	// Signal that SDK was initiated.
+	do_action( 'sk_fs_loaded' );
+}
+
 /**
  * Fire up plugin instance.
  *
