464bd57218420bfb7330aaab32586abc852c3136
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-11-25 16:10:45 +0530

    Add changelog

--- analogwp-templates.php
+++ analogwp-templates.php
@@ -16,8 +16,8 @@
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ang
- * Elementor tested up to: 3.25.7
- * Elementor Pro tested up to: 3.25.3
+ * Elementor tested up to: 3.25.10
+ * Elementor Pro tested up to: 3.25.4
  */
 
 defined( 'ABSPATH' ) || exit;
