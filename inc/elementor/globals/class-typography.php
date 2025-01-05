e3bbdbafeecfe4213e260a3389514c4b4a935f08
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-01-05 11:05:27 +0530

    Modify Global Colors and Fonts API to include both kit results

--- inc/elementor/globals/class-typography.php
+++ inc/elementor/globals/class-typography.php
@@ -15,41 +15,62 @@ class Typography extends Base {
 		return 'globals/typography/{id}';
 	}
 
-	protected function get_kit_items() {
+	/**
+	 * Adds global kit fonts if page kit font is not set.
+	 *
+	 * @param $result
+	 * @param $kit_result
+	 * @return mixed
+	 */
+	protected function get_set_fonts( $result, $kit_result ) {
+		foreach ( $kit_result as $key => $value ) {
+			$font_value = $value['value'];
+			if ( 'text' !== $key ) {
+				continue;
+			}
+			if ( isset( $font_value['typography_typography'] ) && 'custom' === $font_value['typography_typography'] ) {
+				if ( ! isset( $result[ $key ] ) ) {
+					$result[ $key ] = $value;
+					continue;
+				}
+
+				$old_value      = $result[ $key ];
+				$old_font_value = $old_value['value'];
+
+				if ( ! isset( $old_font_value['typography_typography'] ) || 'custom' !== $old_font_value['typography_typography'] ) {
+					$result[ $key ] = $value;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	protected function get_kit_fonts( $kit ) {
 		$result = array();
 
-		$global_kit = Plugin::elementor()->kits_manager->get_active_kit_for_frontend();
+		if ( ! $kit && method_exists( $kit, 'get_id' ) && ! Plugin::elementor()->kits_manager->is_kit( $kit->get_id() ) ) {
+			return $result;
+		}
 
 		// Use raw settings that doesn't have default values.
-		$kit_raw_settings = $global_kit->get_data( 'settings' );
+		$kit_raw_settings = $kit->get_data( 'settings' );
 
 		if ( isset( $kit_raw_settings['system_typography'] ) ) {
 			$system_items = $kit_raw_settings['system_typography'];
 		} else {
 			// Get default items, but without empty defaults.
-			$control      = $global_kit->get_controls( 'system_typography' );
+			$control      = $kit->get_controls( 'system_typography' );
 			$system_items = $control['default'];
 		}
 
-		$custom_items = $global_kit->get_settings( 'custom_typography' );
+		$custom_items = $kit->get_settings( 'custom_typography' );
 
 		if ( ! $custom_items ) {
 			$custom_items = array();
 		}
 
 		$items = array_merge( $system_items, $custom_items );
-
-		// Custom hack for getting the active kit on page.
-		$current_page_id = Options::get_instance()->get( 'ang_current_page_id' );
-		$kit             = false;
-		if ( $current_page_id ) {
-			$kit = Utils::get_document_kit( $current_page_id );
-		}
-
-		// Fallback to global kit.
-		if ( ! $kit ) {
-			$kit = $global_kit;
-		}
 
 		$font_keys = array(
 			'ang_global_title_fonts',
@@ -98,6 +119,37 @@ class Typography extends Base {
 			unset( $item['_id'], $item['title'] );
 
 			$result[ $id ]['value'] = $item;
+		}
+
+		return $result;
+	}
+
+	protected function get_kit_items() {
+		$global_kit = Plugin::elementor()->kits_manager->get_active_kit_for_frontend();
+
+		// Whether to also get the globals data.
+		$also_inline_global_kit = Options::get_instance()->get( 'also_inline_global_kit' );
+
+		// Custom hack for getting the active kit on page.
+		$current_page_id = Options::get_instance()->get( 'ang_current_page_id' );
+		$kit             = false;
+
+		if ( $current_page_id ) {
+			$kit = Utils::get_document_kit( $current_page_id );
+		}
+
+		// Fallback to global kit.
+		if ( ! $kit ) {
+			$kit = $global_kit;
+		}
+
+		$result = $this->get_kit_fonts( $kit );
+
+		if ( $also_inline_global_kit ) {
+			// In case there is a page kit we add the global kit data.
+			$global_kit_result = $this->get_kit_fonts( $global_kit );
+			$result            = array_merge( $global_kit_result, $result );
+			$result            = $this->get_set_fonts( $result, $global_kit_result );
 		}
 
 		return $result;
