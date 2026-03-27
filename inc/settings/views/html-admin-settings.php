39a86d8da5cca4f9777139c377023078c200bdf7
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2026-03-27 16:01:58 +0530

    Update settings sidebar

--- inc/settings/views/html-admin-settings.php
+++ inc/settings/views/html-admin-settings.php
@@ -8,6 +8,8 @@
 
 namespace Analog\Settings\views;
 
+use Analog\Utils;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -149,12 +151,11 @@ if ( ! $tab_exists ) {
 				<div>
 					<h3>🙋 <?php esc_html_e( 'Looking for help or have a feature request?', 'ang' ); ?></h3>
 				</div>
-				<div class="social">
-					<a href="https://x.com/analogwp" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16z" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg></a>
-					<a href="https://facebook.com/analogwp" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" /></svg></a>
-					<a href="https://instagram.com/analogwp" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16.5 7.5v.01" /></svg></a>
-				</div>
-				<div>
+				<div class="action-buttons">
+					<?php if ( Utils::has_pro() ) : ?>
+						<a class="button button-secondary" href="<?php echo esc_url( admin_url( 'admin.php?page=analogwp_templates-account' ) ); ?>">Account</a>
+					<?php endif; ?>
+					<a class="button button-secondary" href="<?php echo esc_url( admin_url( 'admin.php?page=analogwp_templates-contact' ) ); ?>">Create a Support Request</a>
 					<a class="button button-secondary" href="https://analogwp.com/docs/?utm_source=plugin&utm_medium=referral&utm_campaign=settings-help" target="_blank"><?php esc_html_e( 'Visit Documentation', 'ang' ); ?></a>
 				</div>
 			</div>
